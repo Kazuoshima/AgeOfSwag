cfa25e8cad38d76b0cfbac5a79a560ad472ccc18
paul.roulin <paul.roulin@edu.hefr.ch>
2019-12-17 14:57:02 +0100

    implemented basis of GameCtrl

--- src/GameCtrl.java
+++ src/GameCtrl.java
@@ -1,40 +1,47 @@
+import java.util.Scanner;
+
 public class GameCtrl {
+    static private Player p1;
+    static private Settings settings;
+    static private Player p2;
+    static private int actualRound;
+    private Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("SWAG");
+        System.out.println("Yoyoyo bienvenue sur Age Of Swag");
+        startGame();
     }
 
-	public void fight() {
-		// TODO - implement GameCtrl.fight
-		throw new UnsupportedOperationException();
-	}
+    static private void fight() {
+        // TODO - implement GameCtrl.fight
+        throw new UnsupportedOperationException();
+    }
 
-	public void startGame() {
-		// TODO - implement GameCtrl.startGame
-		throw new UnsupportedOperationException();
-	}
+    static private void startGame() {
+        System.out.println("Voici les paramètres actuels de la partie :");
+        displaySettings();
+        System.out.println("Voulez vous modifier ces paramètres ? [y/N]");
 
-	public void prepareRound() {
-		// TODO - implement GameCtrl.prepareRound
-		throw new UnsupportedOperationException();
-	}
+    }
 
-	public void endGame() {
-		// TODO - implement GameCtrl.endGame
-		throw new UnsupportedOperationException();
-	}
+    static private void prepareRound() {
+        // TODO - implement GameCtrl.prepareRound
+        throw new UnsupportedOperationException();
+    }
 
-	public void displaySettings() {
-		// TODO - implement GameCtrl.displaySettings
-		throw new UnsupportedOperationException();
-	}
+    static private void endGame() {
+        // TODO - implement GameCtrl.endGame
+        throw new UnsupportedOperationException();
+    }
 
-	public void changeSettings() {
-		// TODO - implement GameCtrl.changeSettings
-		throw new UnsupportedOperationException();
-	}
+    static private void displaySettings() {
+        // TODO - implement GameCtrl.displaySettings
+        throw new UnsupportedOperationException();
+    }
 
-	Player p1;
-	Settings settings;
-	Player p2;
-	private int actualRound;
+    static private void changeSettings() {
+        // TODO - implement GameCtrl.changeSettings
+        throw new UnsupportedOperationException();
+    }
+
 }
